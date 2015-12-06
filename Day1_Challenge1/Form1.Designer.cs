5f23691c3beb085f3a33031c7ee55c3b470d0f28
Adam Coulson <adam-c_94@live.com>
2015-12-06 13:21:57 +1100

    Fixed Error from Removing Button from Code
    - Now Solves Puzzle Instantly without Pressing Button

--- Day1_Challenge1/Form1.Designer.cs
+++ Day1_Challenge1/Form1.Designer.cs
@@ -29,7 +29,6 @@
 		private void InitializeComponent()
 		{
 			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-			this.button1 = new System.Windows.Forms.Button();
 			this.label1 = new System.Windows.Forms.Label();
 			this.label2 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
@@ -43,20 +42,10 @@
 			this.richTextBox1.TabIndex = 0;
 			this.richTextBox1.Text = "";
 			// 
-			// button1
-			// 
-			this.button1.Location = new System.Drawing.Point(12, 197);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(75, 23);
-			this.button1.TabIndex = 1;
-			this.button1.Text = "Find Floor";
-			this.button1.UseVisualStyleBackColor = true;
-			this.button1.Click += new System.EventHandler(this.button1_Click);
-			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(93, 202);
+			this.label1.Location = new System.Drawing.Point(12, 202);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(58, 13);
 			this.label1.TabIndex = 2;
@@ -65,7 +54,7 @@
 			// label2
 			// 
 			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(157, 202);
+			this.label2.Location = new System.Drawing.Point(76, 202);
 			this.label2.Name = "label2";
 			this.label2.Size = new System.Drawing.Size(35, 13);
 			this.label2.TabIndex = 3;
@@ -78,7 +67,6 @@
 			this.ClientSize = new System.Drawing.Size(602, 231);
 			this.Controls.Add(this.label2);
 			this.Controls.Add(this.label1);
-			this.Controls.Add(this.button1);
 			this.Controls.Add(this.richTextBox1);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
 			this.Name = "Form1";
@@ -92,7 +80,6 @@
 		#endregion
 
 		private System.Windows.Forms.RichTextBox richTextBox1;
-		private System.Windows.Forms.Button button1;
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Label label2;
 	}
